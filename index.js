be4288d551defc112413f3b43e0f52c14f132226
joerockclimber <joerockclimber@gmail.com>
2018-11-30 16:40:11 -0700

    ajax

--- index.js
+++ index.js
@@ -5,19 +5,6 @@ var bodyParser = require('body-parser');
 var apiKey = '5471ad3689f4376e5e13457053a9a305';
 var request = require('request');
 
-/*how to parse post without framework
-if (req.method === 'POST') {
-    let body = '';
-    req.on('data', chunk => {
-        body += chunk.toString(); // convert Buffer to string
-    });
-    req.on('end', () => {
-        console.log(body);
-        res.end('ok');
-    });
-}
-*/
-
 app.set('port', (process.env.PORT || 5000));
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -58,14 +45,14 @@ app.post('/weather', function (req, res) {
     let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
     request(url, function (err, response, body) {
         if(err){
-            res.render('pages/weather2', {weather: null, error: 'Error, please try again'});
+            res.send('Error, please try again');
         } else {
             let weather = JSON.parse(body)
             if(weather.main == undefined){
-                res.render('pages/weather2', {weather: null, error: 'Error, please try again'});
+                res.send('Error, please try again');
             } else {
                 let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
-                res.render('pages/weather2', {weather: weatherText, error: null});
+                res.send( weatherText);
             }
         }
     });
@@ -89,7 +76,6 @@ app.post('/5weather', function (req, res) {
             } else {
                 console.log(JSON.stringify(weather.list));
                 res.render('pages/weather3', {weather: weather.list, city: weather.city.name, error: null});
-                //weather.list.dt_txt // weather.list.main.temp //dt_txt //temp // city //JSON.stringify()
             }
         }
     });
@@ -113,36 +99,9 @@ app.post('/5weather', function (req, res) {
 
 
 
-/*var c1 = cookie.serialize(key1, value1, {httpOnly: true, path: '/', signed: true});
-var c2 = cookie.serialize(key2, value2, {httpOnly: true, path: '/', signed: true});
-res.setHeader('Set-Cookie', weather1);
-res.append('Set-Cookie', weather2);*/
-
-/*    res.setHeader('Set-Cookie', cookie.serialize('name', String(query.name), {
-      httpOnly: true,
-      maxAge: 60 * 60 * 24 * 7 // 1 week
-    }));*/
-
-
-/*cookie{weather{temp, city}}*/
 
 
 
-/* var serializeCookie = function(key, value, hrs) {
- // This is res.cookie’s code without the array management and also ignores signed cookies.
- if ('number' == typeof value) value = val.toString();
- if ('object' == typeof value) value = JSON.stringify(val);
- return cookie.serialize(key, value, { expires: new Date(Date.now() + 1000 * 60 * hrs), httpOnly: true });
-};*/
-
-/* var setMultipleCookies = function(res) {
-    set_cookies.push(getCookie(myCookie1, myValue1.toString(), default_cookie_age);
-    set_cookies.push(getCookie(myCookie2, myValue2.toString(), default_cookie_age);
-    res.header("Set-Cookie", set_cookies);
-}*/
-
-///addcity
-///delete city
 
 
 
